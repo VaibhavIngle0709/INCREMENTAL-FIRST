ad41047438ac8a7fa6849d80b16231fb187fd89a
Iamneo <techadmin@iamneo.ai>
2023-11-07 11:36:31 +0000

    Backup Commit

---
+++ dotnetapp/Managers/PlayerManager.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dotnetapp.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace dotnetapp.Managers
+{
+    public class PlayerManager
+    {
+        // Write your fuctions here...
+        // DisplayAllPlayers
+        // AddPlayer
+        // EditPlayer
+        // DeletePlayer
+        // ListPlayers
+        // FindPlayer
+        // DisplayAllPlayers
+
+        public void AddPlayerToDatabase(Player p)
+        {
+
+        }
+        public void AddPlayer()
+        {
+
+        }
+        public void EditPlayer(int id)
+        {
+
+        }
+        public void DeletePlayer(int id)
+        {
+
+        }
+         public void ListPlayers()
+        {
+
+        }
+        public void FindPlayer(int id)
+        {
+
+        }
+        public void DisplayAllPlayers()
+        {
+
+        }
+
+
+    }
+}
---
+++ dotnetapp/Models/Player.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetapp.Models
+{
+    public class Player
+    {
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public int Age{get;set;}
+        public string Catergory{get;set;}
+        public decimal BiddingPrice{get;set;}
+        public Player(){}
+
+        // Write your Player class here...        
+    }
+}
