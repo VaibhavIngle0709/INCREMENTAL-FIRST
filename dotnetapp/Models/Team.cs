b29cf1e313ab3201bebd460940736246d46a1e09
Iamneo <techadmin@iamneo.ai>
2023-11-12 17:32:28 +0000

    Backup Commit

--- dotnetapp/Models/Team.cs
+++ dotnetapp/Models/Team.cs
@@ -9,6 +9,8 @@ namespace dotnetapp.Models
     {
         public int TeamId{get;set;}
         public string TeamName{get;set;}
+
+        public List<Player> Players{get;set;}
         // Write your Team class here...        
     }
 }
