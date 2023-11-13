47d02b17ca9ea77f9f0c9948726e7fd96a7a148d
Iamneo <techadmin@iamneo.ai>
2023-11-13 11:32:54 +0000

    Backup Commit

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -10,13 +10,33 @@ namespace dotnetapp
         static void Main(string[] args)
         {
             // Create instances of managers    
-            Console.WriteLine("Welcome To IPL Bidding");
-            Console.WriteLine("1.Team Management.");
-            Console.WriteLine("2.Player Management."); 
-            Console.Write("Select You Choice: ");
+            Console.WriteLine("WELCOME TO IPL BIDDING");
+            Console.WriteLine("1.TEAM MANAGEMENT.");
+            Console.WriteLine("2.PLAYER MANAGEMENT."); 
+            Console.Write("SELECT YOUR CHOICE: ");
             int choice =int.Parse(Console.ReadLine());   
             if(choice==1)
             {
+                Console.WriteLine("TEAM MANAGEMENT");
+                Console.WriteLine("1.CREATE TEAM");
+                Console.WriteLine("2.EDIT TEAM");
+                Console.WriteLine("3.DELETE TEAM");
+                Console.WriteLine("4.LIST TEAM");
+                Console.Write("ENTER YOU CHOICE");
+                int teamchoice=int.Parse(Console.ReadLine());
+                TeamManager teamManager=new TeamManager();
+                switch(teamchoice)
+                {
+                    case 1:
+                    teamManager.CreateTeam();
+                    break;
+
+                    case 2:
+                    teamManager.
+                    
+                }
+
+
 
             }
             else if(choice==2)
