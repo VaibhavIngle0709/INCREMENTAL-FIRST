a192797cac16c51b057c69acce59ba7974e31191
Iamneo <techadmin@iamneo.ai>
2023-11-13 12:02:54 +0000

    Backup Commit

--- dotnetapp/Managers/TeamManager.cs
+++ dotnetapp/Managers/TeamManager.cs
@@ -14,9 +14,21 @@ namespace dotnetapp.Managers
         // UpdateTeam
         // DeleteTeam
         // ListTeams
+        private readonly string Connectionstring="User ID=sa;password=examlyMssql@123; server=localhost;Database=IPLDB;trusted_connection=false;Persist Security Info=False;Encrypt=False";
 
         public void CreateTeam()
         {
+            try
+            {
+                SqlConnection con=new SqlConnection(Connectionstring);
+                con.Open();
+                Console.
+                Console.WriteLine("CREATING NEW TEAM");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
         }
         public void EditTeam(int TeamId)
